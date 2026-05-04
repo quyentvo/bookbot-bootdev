4bb8b27ec02fb97fcec2b6c520aeaba4a2de1f74
Quyen Vo <qtvo27@gmail.com>
2026-05-03 21:11:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ def main():
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
+    # gets path based on arg when opening main.py
     book_text = get_book_text(sys.argv[1])
     words = get_words(book_text)
     print(f"Found {words} total words")
+
+    # gets dict of letters within string, then sort it
     characters = get_characters(book_text)
     sorted_characters = sort_list(characters)
+    
+    # checks .isalpha(), only visual printing, don't modify dict
     for char in sorted_characters:
         if char["char"].isalpha():
             print(f"{char['char']}: {char['num']}")
